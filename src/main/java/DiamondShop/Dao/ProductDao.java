e9257b367d104f0809e7b7c685fd98c9accd909f
kieutam102001 <kieutam102001@gmail.com>
2023-08-09 13:00:49 +0700

    Add DAO

---
+++ src/main/java/DiamondShop/Dao/ProductDao.java
@@ -0,0 +1,78 @@
+package DiamondShop.Dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import DiamondShop.Dto.ProductDto;
+import DiamondShop.Dto.ProductDtoMapper;
+
+
+@Repository
+public class ProductDao extends BaseDao{
+	
+	private final boolean YES = true;
+	private final boolean NO = false;
+	
+	private StringBuffer SqlString() {
+		StringBuffer sql = new StringBuffer();
+		sql.append("SELECT ");
+		sql.append("p.id as id_product ");
+		sql.append(", p.id_category ");
+		sql.append(", p.sizes ");
+		sql.append(", p.name ");
+		sql.append(", p.price ");
+		sql.append(", p.sale ");
+		sql.append(", p.title ");
+		sql.append(", p.highlight ");
+		sql.append(", p.new_product ");
+		sql.append(", p.details ");
+		sql.append(", c.id as id_color ");
+		sql.append(", c.name as name_color ");
+		sql.append(", c.code as code_color ");
+		sql.append(", c.img ");
+		sql.append(", p.created_at ");
+		sql.append(", p.updated_at ");
+		sql.append("FROM ");
+		sql.append("products AS p ");
+		sql.append("INNER JOIN ");
+		sql.append("colors AS c ");
+		sql.append("ON p.id = c.id_product ");
+		return sql;
+	}
+	
+	private String SqlProducts(boolean newProduct, boolean highLight) {
+		StringBuffer sql = SqlString();
+		sql.append("WHERE 1 = 1 ");
+		if (highLight) {
+			sql.append("AND p.highlight = true ");
+		}
+		if (newProduct) {
+			sql.append("AND p.new_product = true ");
+		}
+		sql.append("GROUP BY p.id, c.id_product ");
+		sql.append("ORDER BY RAND() ");
+		if (highLight) {
+			sql.append("LIMIT 9 ");
+		}
+		if (newProduct) {
+			sql.append("LIMIT 12 ");
+		}
+		return sql.toString();
+	}
+	
+	public List<ProductDto> GetDataNewProduct() {
+		String sql = SqlProducts(YES, NO);
+		List<ProductDto> list = new ArrayList<ProductDto>();
+		list = _jdbcTemplate.query(sql, new ProductDtoMapper());
+		return list;
+	}
+	
+	public List<ProductDto> GetDataHightlightProduct() {
+		String sql = SqlProducts(NO,YES);
+		List<ProductDto> list = new ArrayList<ProductDto>();
+		list = _jdbcTemplate.query(sql, new ProductDtoMapper());
+		return list;
+	}
+}
--- src/main/java/DiamondShop/Dao/SlidesDao.java
+++ src/main/java/DiamondShop/Dao/SlidesDao.java
@@ -3,24 +3,20 @@ package DiamondShop.Dao;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import DiamondShop.Entity.MapperSlides;
 import DiamondShop.Entity.Slides;
 
+
 @Repository
-public class SlidesDao {
+public class SlidesDao extends BaseDao{
 	
-	@Autowired
-	public JdbcTemplate _jdbcTemplate;
-	
-	public List<Slides>  GetDataSlides(){
+	public List<Slides> GetDataSlides() {
 		List<Slides> list = new ArrayList<Slides>();
 		String sql = "SELECT * FROM slides";
 		list = _jdbcTemplate.query(sql, new MapperSlides());
-		return list;	
+		return list;
 	}
 	
 	
